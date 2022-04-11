def993b88acf106994fee146fca00c4505bfb526
Ayrton <ayrton_ito@hotmail.com>
2022-04-10 21:14:30 -0300

    P2: Dispatcher

--- SIstemas Operacionais/PPOS/source/ppos-core.c
+++ SIstemas Operacionais/PPOS/source/ppos-core.c
@@ -7,9 +7,17 @@
 /*                                   Globals                                  */
 /* -------------------------------------------------------------------------- */
 int task_index = 1;
-task_t main_task;
+task_t main_task, dispatcher_task;
 task_t *current_task;
-queue_t *ready;
+// Queues
+task_t *ready, *executing, *suspended, *terminated;
+
+/* -------------------------------------------------------------------------- */
+/*                                  Functions                                 */
+/* -------------------------------------------------------------------------- */
+task_t *scheduler();
+
+void dispatcher();
 
 /* -------------------------------------------------------------------------- */
 /*                               Implementation                               */
@@ -30,8 +38,8 @@ void ppos_init() {
     current_task = &main_task;
     current_task->state = PPOS_TASK_STATE_EXECUTING;
 
-    // Insert main task in the ready queue
-    queue_append((queue_t **) &ready, (queue_t *) &main_task);
+    // Create dispatcher task with task_create
+    task_create(&dispatcher_task, (void *) dispatcher, NULL);
 
     // Init done
 #ifdef DEBUG
@@ -43,9 +51,6 @@ int task_create(task_t *task, void (*start_func)(void *), void *arg) {
     char *stack;
 
     if (task) {
-        // Update task state
-        task->state = PPOS_TASK_STATE_NEW;
-
         // Save task context
         getcontext(&(task->context));
 
@@ -64,12 +69,17 @@ int task_create(task_t *task, void (*start_func)(void *), void *arg) {
         // Make context
         makecontext(&(task->context), (void *) start_func, 1, arg);
 
-        // If nothing went wrong, set up the task and append to ready queue
+        // If nothing went wrong, set up the task
         task->id = task_index++;
         task->next = NULL;
         task->prev = NULL;
-        task->state = PPOS_TASK_STATE_READY;
-        queue_append((queue_t **) &ready, (queue_t *) task);
+        task->state = PPOS_TASK_STATE_NEW;
+
+        // If not the dispatcher task, insert in the ready queue
+        if (task != &dispatcher_task) {
+            task->state = PPOS_TASK_STATE_READY;
+            queue_append((queue_t **) &ready, (queue_t *) task);
+        }
 
 #ifdef DEBUG
         printf("[PPOS-CORE|CREATE]: Task created with id: %d\n", task->id);
@@ -82,16 +92,19 @@ int task_create(task_t *task, void (*start_func)(void *), void *arg) {
 }
 
 void task_exit(int exitCode) {
-    // Update current task state and exit code
-    current_task->state = PPOS_TASK_STATE_TERMINATED;
-    current_task->exitCode = exitCode;
+    // If the current task is the dispatcher task, switch to main, else switch to dispatcher
+    if (current_task == &dispatcher_task) {
+        task_switch(&main_task);
+    } else {
+        // Update current task state and exit code
+        current_task->state = PPOS_TASK_STATE_TERMINATED;
+        current_task->exitCode = exitCode;
 
 #ifdef DEBUG
-    printf("[PPOS-CORE|exit]: The task %d will be terminated.\n", current_task->id);
+        printf("[PPOS-CORE|EXIT]: The task %d will be terminated.\n", current_task->id);
 #endif
-
-    // Switch to main task so the queue can be processed
-    task_switch(&main_task);
+        task_switch(&dispatcher_task);
+    }
 }
 
 int task_switch(task_t *task) {
@@ -119,4 +132,55 @@ int task_switch(task_t *task) {
 
 int task_id() {
     return current_task->id;
+}
+
+void task_yield() {
+    // Update current task state
+    current_task->state = PPOS_TASK_STATE_READY;
+    // Switch to dispatcher task
+    task_switch(&dispatcher_task);
+}
+
+task_t *scheduler() {
+    // If there are tasks in the ready queue
+    if (queue_size((queue_t *) ready) > 0) {
+        return ready;   //FCFS
+    }
+    return NULL;
+}
+
+void dispatcher() {
+    task_t *task;
+
+    // Dispatcher runs in a loop
+    while (1) {
+        // Get the task from the scheduler
+        task = scheduler();
+
+        // if valid task
+        if (task) {
+            // Remove the task from the ready queue and switch to it
+            queue_remove((queue_t **) &ready, (queue_t *) task);
+            task_switch(task);
+
+            // Insert the task in the proper queue
+            switch (task->state) {
+                case PPOS_TASK_STATE_READY:
+                    queue_append((queue_t **) &ready, (queue_t *) task);
+                    break;
+                case PPOS_TASK_STATE_EXECUTING:
+                    queue_append((queue_t **) &executing, (queue_t *) task);
+                    break;
+                case PPOS_TASK_STATE_SUSPENDED:
+                    queue_append((queue_t **) &suspended, (queue_t *) task);
+                    break;
+                case PPOS_TASK_STATE_TERMINATED:
+                    queue_append((queue_t **) &terminated, (queue_t *) task);
+                    break;
+            }
+        } else {
+            break;
+        }
+    }
+    task_exit(0);
 }
