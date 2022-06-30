eee37b417db91b44dab7a98326824d944d8022d6
Ayrton <ayrton_ito@hotmail.com>
2022-06-30 00:16:41 -0300

    PPOS ajustes no cmake

--- SIstemas Operacionais/PPOS/cmake/FindPPOS.cmake
+++ SIstemas Operacionais/PPOS/cmake/FindPPOS.cmake
@@ -1,12 +1,12 @@
 set(FIND_PPOS_PATHS
-        ~/CLionProjects/PPOS/library)
+        ${PROJECT_SOURCE_DIR}/library)
 
 find_path(PPOS_INCLUDE_DIR
-            disk.h ppos.h
-            ppos-core-globals.h
-            ppos_data.h
-            ppos_disk.h
-            queue.h
+        disk.h ppos.h
+        ppos-core-globals.h
+        ppos_data.h
+        ppos_disk.h
+        queue.h
         PATH_SUFFIXES include
         PATHS ${FIND_PPOS_PATHS})
 
